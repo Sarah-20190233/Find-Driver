6b3ad213b1d0747b51a11eaebfe06bc7ccb4b117
sara-alaa634 <82237076+sara-alaa634@users.noreply.github.com>
2021-11-24 14:03:12 +0300

    Add files via upload

---
+++ Rating.java
@@ -0,0 +1,20 @@
+package ass1_software2;
+
+public abstract class Rating
+ {
+	
+	private  int arr[]=new int[5];
+	public abstract int get_rate();
+	public Rating() {
+	
+		for(int i=0;i<5;i++) {
+			arr[i]=i+1;
+			System.out.println(arr[i]);
+		}
+	}
+	public static void main(String[] args) {
+		Rating r;
+
+	}
+
+}
---
+++ Ride.java
@@ -0,0 +1,41 @@
+package ass1_software2;
+
+public class Ride {
+
+	private String Source;
+	private String Destination;
+    private double Price;
+   
+    public double show_offer() {
+    //	Driver d;
+    //	d.set_offer;
+    	return 0;
+    }
+	
+	public String getSource() {
+		return Source;
+	}
+
+	public void setSource(String source) {
+		Source = source;
+	}
+
+	public String getDestination() {
+		return Destination;
+	}
+
+	public void setDestination(String destination) {
+		Destination = destination;
+	}
+
+	public double getPrice() {
+		return Price;
+	}
+
+	public void setPrice(double price) {
+		Price = price;
+	}
+
+
+
+}
