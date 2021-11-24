d0df1e7693b0f445eb12fc5dfd39e5e46018c300
sara-alaa634 <82237076+sara-alaa634@users.noreply.github.com>
2021-11-25 00:12:28 +0300

    Updated

---
+++ User.java
@@ -0,0 +1,13 @@
+package software2;
+
+interface User
+{
+    public void Login(String email , String password);
+    public void Register(String userName , String email , String password , String phone);
+    
+}
+
+
+
+
+
