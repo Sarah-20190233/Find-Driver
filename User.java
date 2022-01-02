71ee9322331e3f0b2e6dc6c5a5fd6a9da7bfdeb2
Sara ALaa <zaa3748@gmail.com>
2022-01-02 13:43:06 +0200

    updated

--- User.java
+++ User.java
@@ -1,10 +1,8 @@
-package software2;
-
+package sprint2;
 interface User
 {
-    public void Login(String email , String password);
     public void Register(String userName , String email , String password , String phone);
-    
+    public void Login(String email , String password);    
 }
 
 
